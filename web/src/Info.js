a5c2709290b7b4408be1dfa014f37b483b5793bd
Pete Gadomski <pete.gadomski@gmail.com>
2021-02-11 10:16:51 -0700

    Status page is looking good

--- web/src/Info.js
+++ web/src/Info.js
@@ -1,5 +1,17 @@
+import Loading from './Loading';
+import Table from 'react-bootstrap/Table'
+
 function Info(props) {
-    return null;
+    if (props.status.isLoaded) {
+        const sensorInfo = props.status.data.sensor_info;
+        const sensorInfoRows = Object.keys(sensorInfo).map((k, i) => <tr key={i}><th>{k}</th><td>{sensorInfo[k]}</td></tr>);
+        return <div>
+            <h2>Sensor info</h2>
+            <Table className="col-md-6"><tbody>{sensorInfoRows}</tbody></Table>
+        </div>;
+    } else {
+        return <Loading />
+    }
 }
 
 export default Info;
