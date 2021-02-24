eba1330f790990b8bb57c6afe1c77194c615b496
Pete Gadomski <pete.gadomski@gmail.com>
2021-02-24 08:19:29 -0700

    Add client code

--- server/src/client.rs
+++ server/src/client.rs
@@ -1,92 +1,16 @@
-use anyhow::{Error, anyhow};
-use serde_json::Value;
-use std::fmt;
-use tokio::{
-    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
-    net::{TcpStream, ToSocketAddrs},
-};
+use std::io::Result as IoResult;
+use tokio::net::{TcpStream, ToSocketAddrs};
 
-#[derive(Debug)]
+/// A client for talking to an Ouster scanner.
 pub struct Client {
     stream: TcpStream,
 }
 
-#[derive(Debug, thiserror::Error)]
-pub struct EndOfFile;
-
 impl Client {
-    pub async fn connect<A: ToSocketAddrs>(addr: A) -> Result<Client, Error> {
+    /// Connect to an Ouster client.
+    pub async fn connect<A: ToSocketAddrs>(addr: A) -> IoResult<Client> {
         TcpStream::connect(addr)
             .await
-            .map(|s| Client { stream: s })
-            .map_err(Error::from)
-    }
-
-    pub async fn get(&mut self, key: &str) -> Result<Value, Error> {
-        log::debug!("getting {}", key);
-        let query = format!("get_{}\n", key);
-        let key: Vec<u8> = query.bytes().collect();
-        self.stream.write_all(&key).await?;
-        let mut response = String::new();
-        let mut stream = BufReader::new(&mut self.stream);
-        let count = stream.read_line(&mut response).await?;
-        if count == 0 {
-            log::warn!("end of file");
-            return Err(Error::from(EndOfFile));
-        }
-        log::debug!("received response: {}", response);
-        if response.starts_with("error") {
-            Err(anyhow!("ouster client {}", response.trim()))
-        } else {
-            serde_json::from_str(&response).map_err(Error::from)
-        }
-    }
-
-    pub async fn set(&mut self, key: &str, value: &str) -> Result<(), Error> {
-        log::debug!("setting {}={}", key, value);
-        let command: Vec<u8> = format!("set_config_param {} {}\n", key, value).bytes().collect();
-        self.stream.write_all(&command).await?;
-        let mut response = String::new();
-        let mut stream = BufReader::new(&mut self.stream);
-        let count = stream.read_line(&mut response).await?;
-        if count == 0 {
-            log::warn!("end of file");
-            return Err(Error::from(EndOfFile));
-        }
-        log::debug!("received response: {}", response);
-        if response.starts_with("error") {
-            Err(anyhow!("ouster client {}", response.trim()))
-        } else if response.trim() != "set_config_param" {
-            Err(anyhow!("response {} does not match {}", response.trim(), "set_config_param"))
-        } else {
-            Ok(())
-        }
-    }
-
-    pub async fn reinitialize(&mut self) -> Result<(), Error> {
-        log::debug!("reinitializing");
-        let command: Vec<u8> = format!("reinitialize\n").bytes().collect();
-        self.stream.write_all(&command).await?;
-        let mut response = String::new();
-        let mut stream = BufReader::new(&mut self.stream);
-        let count = stream.read_line(&mut response).await?;
-        if count == 0 {
-            log::warn!("end of file");
-            return Err(Error::from(EndOfFile));
-        }
-        log::debug!("received response: {}", response);
-        if response.starts_with("error") {
-            Err(anyhow!("ouster client {}", response.trim()))
-        } else if response.trim() != "reinitialize" {
-            Err(anyhow!("response {} does not match {}", response.trim(), "reinitialize"))
-        } else {
-            Ok(())
-        }
-    }
-}
-
-impl fmt::Display for EndOfFile {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        writeln!(f, "end of file")
+            .map(|stream| Client { stream })
     }
 }
--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,10 +1,3 @@
-pub mod filters;
-pub mod handlers;
-
 mod client;
-mod config;
-mod manager;
 
 pub use client::Client;
-pub use config::Config;
-pub use manager::{Command, Manager};
