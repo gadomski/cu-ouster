68b45f05f5fa8eae43ec72a1b27879a1baa78cd8
Pete Gadomski <pete.gadomski@gmail.com>
2021-02-25 12:02:18 -0700

    Remove web app

--- web/src/Dashboard.js
+++
@@ -1,5 +0,0 @@
-function Home(props) {
-    return null;
-}
-
-export default Home;
--- web/src/Loading.js
+++
@@ -1,7 +0,0 @@
-import Spinner from 'react-bootstrap/Spinner';
-
-function Loading() {
-    return <span><Spinner animation="border" className="mr-2" size="sm" as="span" />Loading...</span>;
-}
-
-export default Loading;
